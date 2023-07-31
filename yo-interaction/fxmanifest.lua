9d3092dbc62cb21765b032f360c3bcd9784a6f1a
Orex1 <140979909+Orex1@users.noreply.github.com>
2023-07-31 23:48:31 +0300

    New Version
    New Version of the script!

---
+++ yo-interaction/fxmanifest.lua
@@ -0,0 +1,16 @@
+--https://discord.gg/JaX9Ed9jdD 
+--YO Shop Join Now!
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+
+client_scripts {
+    'client.lua',
+    'config.lua',
+    'keys.lua'
+}
+
+ui_page 'html/index.html'
+files {
+    'html/*',
+}
